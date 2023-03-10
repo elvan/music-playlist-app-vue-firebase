133bdcaa7a07309481ad5ff2978b13409ecd4704
Elvan Hidayat <elvanhd@gmail.com>
2023-03-10 13:50:31 +0700

    uploading cover images

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
+
 import firebaseConfig from './firebaseConfig';
 
 // init firebase
