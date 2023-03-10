2d25ecbfc6fe852994cca63e09af35c0c0272cd0
Elvan Hidayat <elvanhd@gmail.com>
2023-03-10 14:03:59 +0700

    adding new playlists

--- src/views/playlists/CreatePlaylist.vue
+++ src/views/playlists/CreatePlaylist.vue
@@ -18,28 +18,49 @@
       <input type="file" @change="handleChange" />
       <div class="error">{{ fileError }}</div>
 
-      <button>Create</button>
+      <button v-if="!isPending">Create</button>
+      <button v-else disabled>Saving...</button>
     </form>
   </div>
 </template>
 
 <script>
+import getUser from '@/composables/getUser';
+import useCollection from '@/composables/useCollection';
 import useStorage from '@/composables/useStorage';
+import { timestamp } from '@/firebase/config';
 import { ref } from 'vue';
 
 export default {
   setup() {
     const { filePath, url, uploadImage } = useStorage();
+    const { error, addDoc } = useCollection('playlists');
+    const { user } = getUser();
 
     const title = ref('');
     const description = ref('');
     const file = ref(null);
     const fileError = ref(null);
+    const isPending = ref(false);
 
     const handleSubmit = async () => {
       if (file.value) {
+        isPending.value = true;
         await uploadImage(file.value);
-        console.log('image uploaded, url: ', url.value);
+        await addDoc({
+          title: title.value,
+          description: description.value,
+          userId: user.value.uid,
+          userName: user.value.displayName,
+          coverUrl: url.value,
+          filePath: filePath.value, // so we can delete it later
+          songs: [],
+          createdAt: timestamp(),
+        });
+        isPending.value = false;
+        if (!error.value) {
+          console.log('playlist added');
+        }
       }
     };
 
@@ -59,7 +80,14 @@ export default {
       }
     };
 
-    return { title, description, handleSubmit, fileError, handleChange };
+    return {
+      title,
+      description,
+      handleSubmit,
+      fileError,
+      handleChange,
+      isPending,
+    };
   },
 };
 </script>
