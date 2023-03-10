bab0b4790ec889812719a36ed772b821eae5d96e
Elvan Hidayat <elvanhd@gmail.com>
2023-03-11 00:18:04 +0700

    redirecting to playlist-details

--- src/views/playlists/CreatePlaylist.vue
+++ src/views/playlists/CreatePlaylist.vue
@@ -30,12 +30,14 @@ import useCollection from '@/composables/useCollection';
 import useStorage from '@/composables/useStorage';
 import { timestamp } from '@/firebase/config';
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default {
   setup() {
     const { filePath, url, uploadImage } = useStorage();
     const { error, addDoc } = useCollection('playlists');
     const { user } = getUser();
+    const router = useRouter();
 
     const title = ref('');
     const description = ref('');
@@ -47,7 +49,7 @@ export default {
       if (file.value) {
         isPending.value = true;
         await uploadImage(file.value);
-        await addDoc({
+        const res = await addDoc({
           title: title.value,
           description: description.value,
           userId: user.value.uid,
@@ -59,7 +61,7 @@ export default {
         });
         isPending.value = false;
         if (!error.value) {
-          console.log('playlist added');
+          router.push({ name: 'PlaylistDetails', params: { id: res.id } });
         }
       }
     };
