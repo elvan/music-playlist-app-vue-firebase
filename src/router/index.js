682b2dcbd8387aedb5d2959d277f8769b2953582
Elvan Hidayat <elvanhd@gmail.com>
2023-03-11 06:06:02 +0700

    user playlists route

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Signup from '../views/auth/Signup.vue';
 import Home from '../views/Home.vue';
 import CreatePlaylist from '../views/playlists/CreatePlaylist.vue';
 import PlaylistDetails from '../views/playlists/PlaylistDetails.vue';
+import UserPlaylists from '../views/playlists/UserPlaylists.vue';
 
 // route guard
 import { projectAuth } from '../firebase/config';
@@ -45,6 +46,12 @@ const routes = [
     name: 'PlaylistDetails',
     component: PlaylistDetails,
     props: true,
+    beforeEnter: requireAuth,
+  },
+  {
+    path: '/playlists/user',
+    name: 'UserPlaylists',
+    component: UserPlaylists,
     beforeEnter: requireAuth,
   },
 ];
