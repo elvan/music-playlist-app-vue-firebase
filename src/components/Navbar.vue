31b8d1e79424ab3490394d9a6977a93a11c7b440
Elvan Hidayat <elvanhd@gmail.com>
2023-03-10 13:08:16 +0700

    conditionally showing links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,9 +4,13 @@
       <img src="@/assets/logo.png" alt="logo" />
       <h1><router-link :to="{ name: 'Home' }">MusicPlaylist</router-link></h1>
       <div class="links">
-        <button @click="handleClick">Logout</button>
-        <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
-        <router-link class="btn" :to="{ name: 'Login' }">Login</router-link>
+        <div v-if="user">
+          <button @click="handleClick">Logout</button>
+        </div>
+        <div v-else>
+          <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+          <router-link class="btn" :to="{ name: 'Login' }">Login</router-link>
+        </div>
       </div>
     </nav>
   </div>
@@ -14,10 +18,12 @@
 
 <script>
 import { useRouter } from 'vue-router';
+import getUser from '../composables/getUser';
 import useLogout from '../composables/useLogout';
 
 export default {
   setup() {
+    const { user } = getUser();
     const { logout } = useLogout();
     const router = useRouter();
 
@@ -27,7 +33,7 @@ export default {
       router.push({ name: 'Login' });
     };
 
-    return { handleClick };
+    return { handleClick, user };
   },
 };
 </script>
