176af78210cad8c9bf383a59fd508baeaa626372
Elvan Hidayat <elvanhd@gmail.com>
2024-02-14 19:42:25 +0700

    Implement ListView component to display playlists
    - Add ListView component for displaying playlist details
    - Integrate ListView in the Home view to show all playlists
    - Fetch playlists from Firestore using getCollection composable
    - Display playlist cover, title, creator, and song count
    - Style ListView for a visually appealing presentation
    - Implement hover effects for playlist items for better UX
    - Handle errors in Home view if playlists can't be fetched

---
+++ src/components/ListView.vue
@@ -0,0 +1,56 @@
+<template>
+  <div v-for="playlist in playlists" :key="playlist.id">
+    <div class="single">
+      <div class="thumbnail">
+        <img :src="playlist.coverUrl" />
+      </div>
+      <div class="info">
+        <h3>{{ playlist.title }}</h3>
+        <p>created by {{ playlist.userName }}</p>
+      </div>
+      <div class="song-number">
+        <p>{{ playlist.songs.length }} songs</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['playlists'],
+};
+</script>
+
+<style scoped>
+.single {
+  display: flex;
+  align-items: center;
+  margin: 16px 0;
+  padding: 20px;
+  border-radius: 10px;
+  background: white;
+  transition: all ease 0.2s;
+}
+.single:hover {
+  box-shadow: 1px 2px 3px rgba(50, 50, 50, 0.05);
+  transition: all ease 0.2s;
+  transform: scale(1.02);
+}
+.thumbnail {
+  max-width: 128px;
+  max-height: 128px;
+}
+img {
+  object-fit: cover;
+  display: block;
+  max-width: 128px;
+  max-height: 128px;
+  border-radius: 10px;
+}
+.info {
+  margin: 0 30px;
+}
+.song-number {
+  margin-left: auto;
+}
+</style>
